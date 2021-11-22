fb10d80b1dfce6184c50e34d91a6faf1f2f061d5
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2021-11-22 10:39:56 +0100

    revised TD 1

--- ad.jl
+++ ad.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.17.1
+# v0.16.1
 
 using Markdown
 using InteractiveUtils
@@ -269,7 +269,7 @@ uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
 
 [[LinearAlgebra]]
-deps = ["Libdl"]
+deps = ["Libdl", "libblastrampoline_jll"]
 uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
 
 [[LogExpFunctions]]
@@ -308,6 +308,10 @@ version = "0.3.5"
 
 [[NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
 
 [[OpenLibm_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -420,6 +424,10 @@ deps = ["MacroTools"]
 git-tree-sha1 = "8c1a8e4dfacb1fd631745552c8db35d0deb09ea0"
 uuid = "700de1a5-db45-46bc-99cf-38207098b444"
 version = "0.2.2"
+
+[[libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
 
 [[nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
--- linalg.jl
+++ linalg.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.17.1
+# v0.16.1
 
 using Markdown
 using InteractiveUtils
@@ -69,15 +69,6 @@ N =
 # ╔═╡ b92d90ef-9859-482e-ab54-f6aba8b73afd
 # Réponse 4
 
-# ╔═╡ 90007dd8-147f-42bc-96d3-56b34241eb3b
-md"""
-5. Extraire de la matrice ``M`` la matrice ``R`` obtenue en prenant une colonne sur deux.
-
-"""
-
-# ╔═╡ bfac7fd5-c149-4ea4-a026-f96c08686440
-# Réponse 5
-
 # ╔═╡ ca8cb49a-f39c-11ea-307e-97825e864b0f
 md"""
 # Matrices et systèmes linéaires
@@ -110,6 +101,24 @@ x + 2y + 3z + 4t & = 1, \\
 
 """
 
+# ╔═╡ 0345702e-3357-4490-8f81-22a0f09d5efa
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0-rc1"
+manifest_format = "2.0"
+
+[deps]
+"""
+
 # ╔═╡ Cell order:
 # ╟─9a13b940-028f-441a-968b-9e70f9a7a3e5
 # ╠═1273e2aa-c1de-4912-8633-0a1cc4a364f3
@@ -119,8 +128,9 @@ x + 2y + 3z + 4t & = 1, \\
 # ╠═61bc395f-a210-4e13-849a-9e2bbc82e782
 # ╟─03cb7526-f392-11ea-123a-797739ba60de
 # ╠═b92d90ef-9859-482e-ab54-f6aba8b73afd
-# ╟─90007dd8-147f-42bc-96d3-56b34241eb3b
-# ╠═bfac7fd5-c149-4ea4-a026-f96c08686440
 # ╟─ca8cb49a-f39c-11ea-307e-97825e864b0f
 # ╟─46497466-f39e-11ea-2634-cd5985460790
 # ╟─7fb06e78-f39f-11ea-38d9-ab51b94af7c5
+# ╠═0345702e-3357-4490-8f81-22a0f09d5efa
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
